7768ab74f7b70cf81b39cf50bf9d3f300662080b
Trapti10 <traptivishwakarma2007@gmail.com>
2025-11-26 00:00:12 +0530

    Day-3

---
+++ Project_7_video_background/script.js
@@ -0,0 +1,15 @@
+const video = document.querySelector(".bg-video")
+
+const btn  = document.querySelector(".switch-btn")
+
+btn.addEventListener("click", function(){
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide')
+        video.pause()
+    }
+    else{
+        
+        btn.classList.remove('slide')
+        video.play()
+    }
+})
